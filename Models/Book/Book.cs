eac999211b9d744542a565e222ede672427968b8
vizorian <birstonasvilius@gmail.com>
2023-09-27 12:30:56 +0300

    Added advanced validation for ISBN

--- Models/Book/Book.cs
+++ Models/Book/Book.cs
@@ -14,13 +14,13 @@ public class Book
             throw new ValidationException(validationResult.Errors);
         }
 
-        ISBN13 = isbn13;
-        ISBN = isbn;
+        Isbn13 = isbn13;
+        Isbn = isbn;
     }
 
-    public string Id => !string.IsNullOrWhiteSpace(ISBN13) ? ISBN13 : ISBN;
-    public string ISBN13 { get; }
-    public string ISBN { get; }
+    public string Id => !string.IsNullOrWhiteSpace(Isbn13) ? Isbn13 : Isbn;
+    public string Isbn13 { get; }
+    public string Isbn { get; }
     public string Title { get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty;
     public int YearPublished { get; set; }

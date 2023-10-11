2ce1bc54c9b6e00fdef708fbef9e0e366e1ca8c4
vizorian <birstonasvilius@gmail.com>
2023-10-12 01:05:45 +0300

    Fixed issue

--- Persistence/BookDbContext.cs
+++ Persistence/BookDbContext.cs
@@ -3,16 +3,24 @@ namespace MvcTask.Persistence;
 [ExcludeFromCodeCoverage]
 public class BookDbContext : DbContext
 {
+    public BookDbContext()
+    {
+    }
+
+    public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+    {
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Book>()
             .HasIndex(b => b.Isbn13)
             .IsUnique();
-        
+
         modelBuilder.Entity<Book>()
             .HasIndex(b => b.Isbn)
             .IsUnique();
     }
-    
+
     public virtual DbSet<Book> Books { get; set; } = null!;
 }
